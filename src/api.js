9fe6b5bd85d23e37f62470bf842f6d16d90c6c0c
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-07-05 08:06:03 -0400

    created the processWeather function inside of api.js, and updated the fetchWeather

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ const API_KEY = "C34YB34KU24R89EZLUYW2CYBD";
 async function fetchWeather(location) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(
     location
-  )}?unitGroup=metric&key=${API_KEY}&contentType=json`;
+  )}?unitGroup=us&key=${API_KEY}&contentType=json`;
 
   try {
     const response = await fetch(url);
@@ -13,10 +13,25 @@ async function fetchWeather(location) {
     }
 
     const data = await response.json();
-    console.log(data);
+    const simplifiedWeatherData = processWeatherData(data);
+    console.log(simplifiedWeatherData);
+    return simplifiedWeatherData;
   } catch (error) {
     console.error("Error fetching weather data", error);
   }
+}
+
+function processWeatherData(data) {
+  const today = data.days[0];
+
+  return {
+    location: data.address,
+    date: today.datetime,
+    temperature: today.temp,
+    feelsLike: today.feelslike,
+    conditions: today.conditions,
+    icon: today.icon,
+  };
 }
 
 export { fetchWeather };
